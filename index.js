8fb898265fefb1a0a8c6d54a85c82c913f96531f
chandan hm <chandanhm388@gmail.com>
2023-09-21 18:23:16 +0530

    commitin

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const redux = require('redux')
 const createStore = redux.createStore
 
-//action
+//action creator
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
     return {
@@ -12,11 +13,19 @@ function buyCake() {
     }
 }
 
+function buyIceCream() {
+    return{
+        type: BUY_ICECREAM
+    }
+}
+
 //reducer
 // (previousstate, action) => newstate
 
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    // another reducer
+    numOfIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -24,6 +33,12 @@ const reducer = (state = initialState, action) => {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes -1
+        }
+
+        // another reducer
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams -1
         }
 
         default: return state
@@ -36,4 +51,6 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
